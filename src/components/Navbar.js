01f302584f45cc6e419596a6d22237651613ea8a
charles0830 <bhgold830@gmail.com>
2021-01-03 16:59:43 +0530

    Initial Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React,{ Component } from 'react';
 import {Link} from "react-router-dom";
 import logo from '../phonelogo.svg'
+import styled from "styled-components";
 
 export default class Navbar extends Component{
     render() {
@@ -15,17 +16,37 @@ export default class Navbar extends Component{
                 </Link>
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
-                        <Link to="/" className="nav-link">
+                        <Link to="/" className="nav-link text-white">
                             Products
                         </Link>
                     </li>
                 </ul>
                 <Link to='/cart' className="ml-auto">
-                    <button>
-                        <i className='fas fa-cart-plus'>My Cart</i>
-                    </button>
+                    <ButtonContainer>
+                        <span className="mr-2"><i className='fas fa-cart-plus pr-2'/> My Cart</span>
+                    </ButtonContainer>
                 </Link>
             </nav>
         )
     }
 }
+
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--mainWhite);
+  color: var(--mainWhite);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.2s ease-in-out;
+  &:hover{
+    background: var(--mainWhite);
+    color: var(--lightBlue);
+  }
+  &:focus{
+    outline: none;
+  }
+`
