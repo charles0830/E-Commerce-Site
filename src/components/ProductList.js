0955e150d1a518eaf1b21cbe9a3610110a91b324
charles0830 <bhgold830@gmail.com>
2021-01-03 21:53:54 +0530

    Initial Commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,24 @@
 import React,{ Component } from 'react';
 import Product from './Product'
+import Title from "./Title";
+
 export default class ProductList extends Component{
+    state={
+        product : []
+    }
     render() {
         return (
-            <div>
-                <Product/>
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="our" title="Product"/>
+                        <div className="row">
+
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+
         )
     }
 }
