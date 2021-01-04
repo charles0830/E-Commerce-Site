8084c077b97985dcd52234d99ee967daf8be91d6
charles0830 <bhgold830@gmail.com>
2021-01-04 06:11:30 +0530

    Initial Commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React,{ Component } from 'react';
 import Product from './Product'
 import Title from "./Title";
 import {storeProducts} from "../data";
+import {ProductConsumer} from "../context";
 
 export default class ProductList extends Component{
     state={
@@ -14,12 +15,17 @@ export default class ProductList extends Component{
                     <div className="container">
                         <Title name="our" title="Product"/>
                         <div className="row">
-
+                            <ProductConsumer>
+                                {(value)=>{
+                                    return value.products.map( product => {
+                                        return <Product key={product.id} product={product}/>;
+                                    });
+                                }}
+                            </ProductConsumer>
                         </div>
                     </div>
                 </div>
             </React.Fragment>
-
         )
     }
 }
--- src/context.js
+++ src/context.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import Product from "./components/Product";
+import {storeProducts,detailProduct} from "./data";
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+    state={
+        products:storeProducts,
+        detailProduct:detailProduct
+    }
+    handleDetail = ()=>{
+        console.log('Hello from details')
+    }
+    addToCart = ()=>{
+        console.log('Hello from Cart')
+    }
     render() {
         return (
-            <ProductContext.Provider value="hello from context">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetails:this.handleDetail,
+                addToCart:this.addToCart
+            }}>
                 {
                     this.props.children
                 }
