a3c4406c79757b42a98e3b10bb322b2348cf8150
charles0830 <bhgold830@gmail.com>
2021-01-04 07:10:06 +0530

    Initial Commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,9 @@
 import React,{ Component } from 'react';
 import Product from './Product'
 import Title from "./Title";
-import {storeProducts} from "../data";
 import {ProductConsumer} from "../context";
 
 export default class ProductList extends Component{
-    state={
-        product : storeProducts
-    }
     render() {
         return (
             <React.Fragment>
