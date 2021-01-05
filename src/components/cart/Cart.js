fdc47541cb8dad78830d04d952f6e9650d0b6ef2
charles0830 <bhgold830@gmail.com>
2021-01-05 12:08:24 +0530

    Initial Commit

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import Title from "../Title";
 import CartColumns from "./CartColumns"
+import EmptyCart from "./EmptyCart";
+import {ProductConsumer} from "../../context";
 
 class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="Cart"/>
+                <ProductConsumer>
+                    {value => {
+                        const {cart} = value;
+                        if (cart.length>0){
+                            return (
+                                <React.Fragment>
+                                    <Title name="your" title="Cart"/>
+                                    <CartColumns />
+                                </React.Fragment>
+                            )
+                        }else {
+                            return (<EmptyCart/>)
+                        }
+                    }}
+                </ProductConsumer>
+
             </section>
         );
     }
