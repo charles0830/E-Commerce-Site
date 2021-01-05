ed4a4547f15ded189a9ac415cc7a50899cd5f917
charles0830 <bhgold830@gmail.com>
2021-01-05 17:37:46 +0530

    Initial Commit

--- src/context.js
+++ src/context.js
@@ -73,17 +73,30 @@ class ProductProvider extends Component {
 
     }
     removeItem = (id)=>{
-        this.state.cart.find((item) => {
-            if (item.id===id){
-                this.state.cart.delete(item);
+        let tempProduct = [...this.state.products];
+        let tempCart = [...this.state.cart];
+        tempCart = tempCart.filter(item=>item.id!==id);
+        const index = tempProduct.indexOf(this.getItem(id));
+        let removedProduct = tempProduct[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+        this.setState(()=>{
+            return {
+                cart:[...tempCart],
+                products:[...tempProduct]
             }
-        }
-        )
+        },()=>{
+            this.addTotals();
+        })
     }
     clearCart = ()=>{
         this.setState(()=>{
             return {cart:[]};
-        })
+        },()=>{
+           this.setProducts();
+           this.addTotals();
+        });
     }
     addTotals = ()=>{
         let subtotal = 0;
