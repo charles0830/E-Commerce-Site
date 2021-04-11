5e64f1dc659910d52fc7933f9d2b8aa17663ef0e
charles0830 <bhgold830@gmail.com>
2021-04-12 02:06:38 +0530

    Initial Commit

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
+    title: "Nokia 8",
     img: "img/product-1.png",
     price: 10,
     company: "GOOGLE",
@@ -38,7 +38,7 @@ export const storeProducts = [
   },
   {
     id: 4,
-    title: "HTC 10 - White",
+    title: "Vivo V7",
     img: "img/product-4.png",
     price: 18,
     company: "htc",
@@ -86,9 +86,9 @@ export const storeProducts = [
   },
   {
     id: 8,
-    title: "Smashed Iphone",
+    title: "Honor 8c",
     img: "img/product-8.png",
-    price: 2,
+    price: 15,
     company: "apple",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
